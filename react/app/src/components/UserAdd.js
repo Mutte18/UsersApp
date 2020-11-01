3ddc0d7a5f9da1b4aca96a4fd7d88a9944287780
Mattias <mattias.vidhall@hotmail.com>
2020-11-01 21:07:10 +0100

    Change to h4 to keep same style

--- react/app/src/components/UserAdd.js
+++ react/app/src/components/UserAdd.js
@@ -86,7 +86,7 @@ export default class UserAdd extends Component {
 					</div>
 				) : (
 					<div>
-						<h1>Add User</h1>
+						<h4>Add User</h4>
 						<div className="form-group">
 							<label htmlFor="title">Name</label>
 							<input
